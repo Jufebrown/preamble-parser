3ed0c95051a0e0e0e40dda44a07653f24dcf6962
Jufebrown <jufebrown@gmail.com>
2019-03-27 00:28:56 -0500

    add event listener for t-button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import stringToWordArray from './stringToWordArray';
+import doCountForSingleLetter from './doCountForSingleLetter';
+import doCountForTwoLetters from './doCountForTwoLetters';
 import displayWordArray from './displayWordArray';
 
 const preamble = 'We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.';
 
 const wordArray = stringToWordArray(preamble);
+
+const tButton = document.querySelector('.t-button');
+const eButton = document.querySelector('.e-button');
+const tAndEButton = document.querySelector('.t-e-button');
+
+tButton.addEventListener('click', (event) => {
+  const wordCount = doCountForSingleLetter(wordArray, 't', 'beginning');
+});
+
 displayWordArray(wordArray);
