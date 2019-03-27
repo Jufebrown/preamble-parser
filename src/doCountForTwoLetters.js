698411bc335dbe4468c85d6f3e845906049facea
Jufebrown <jufebrown@gmail.com>
2019-03-26 23:47:15 -0500

    refactor for better variable names

--- src/doCountForTwoLetters.js
+++ src/doCountForTwoLetters.js
@@ -1,11 +1,11 @@
 import isLetterAtPosition from './isLetterAtPosition';
 
-export default function doCountForTwoLetters(wordArray, letter1, letter2, position) {
+export default function doCountForTwoLetters(wordArray, beginningLetter, endingLetter) {
   let wordCounter = 0;
   for (let i = 0; i < wordArray.length; i += 1) {
     if (
-      isLetterAtPosition(wordArray[i], letter1, position)
-      && isLetterAtPosition(wordArray[i], letter1, position)
+      isLetterAtPosition(wordArray[i], beginningLetter, 'beginning')
+      && isLetterAtPosition(wordArray[i], endingLetter, 'end')
     ) {
       wordCounter += 1;
     }
